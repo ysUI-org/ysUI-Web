9b9e681aea6f1b910d7c1b4a5e5be8452440158f
KevDoy <KevDoy@users.noreply.github.com>
2024-06-06 22:11:38 -0700

    v0.01 Hello World
    Proof of concept

---
+++ scripts/ysUI-web.js
@@ -0,0 +1,59 @@
+
+// Import ysUI Actions (Buttons and Links)
+    var actionsData;
+        $.getJSON("/data/actions.json", function (json) {
+          actionsData = json;
+        
+    
+    function iterateActions(data) {
+        for (var i = 0, l = data.actions.length; i < l; i++) {
+            var elements = data.actions[i];
+            
+    
+         
+      $(elements.elem).each(function() {
+      var href = $(this).attr('href');
+      var onclick = $(this).attr('onclick');
+      var id = $(this).attr('id');
+      var importClass = $(this).attr('class');
+      var style = $(this).attr('style');
+      var target = $(this).attr('target');
+      if (importClass != undefined) {
+        var classes = elements.class + " " + importClass;
+      } else {
+        var classes = elements.class;
+      };
+     
+      $(this).replaceWith(function() {
+        return $(elements.newelem, {
+          class: classes,
+          href: href,
+          onclick: onclick,
+          id: id,
+          style: style,
+          target: target,
+          html: $(this).html()
+        });
+      });
+    });
+          
+      }
+  }
+    
+    iterateActions(actionsData);
+    });
+
+
+// Import ysUI Icons
+  var iconsData;
+      $.getJSON("/data/icons.json", function (json) {
+        iconsData = json;
+  function iterateIcons(data) {
+      for (var i = 0, l = data.icons.length; i < l; i++) {
+          var elements = data.icons[i];
+          $(elements.elem).replaceWith(elements.repl); 
+    }
+};
+  iterateIcons(iconsData);
+  });
+
